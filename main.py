444cf1d2bd75b981d537b3c44b14808f276c8623
Explore <1989397949@qq.com>
2025-05-05 20:01:42 +0800

    [up] 增加用户性别选择

--- main.py
+++ main.py
@@ -11,10 +11,11 @@ class Gay(QMainWindow):
         self.setFixedSize(480, 300)
         self.setWindowIcon(QIcon("./icon.ico"))
 
-        ver = "v_1.0.0"
+        ver = "v_1.0.1"
 
         self.examine_button = self.findChild(QPushButton, 'examine')
-        self.combobox = self.findChild(QComboBox, 'comboBox')
+        self.user_like_combobox = self.findChild(QComboBox, 'user_like')
+        self.user_gender_combobox = self.findChild(QComboBox, 'user_gender')
         self.ver_label = self.findChild(QLabel, 'ver')
         # 为按钮的点击事件绑定槽函数
         self.examine_button.clicked.connect(self.check_combobox)
@@ -22,13 +23,14 @@ class Gay(QMainWindow):
 
     def check_combobox(self):
         # 获取下拉框当前选中的文本
-        selected_text = self.combobox.currentText()
-        if selected_text == "男":
+        user_like = self.user_like_combobox.currentText()
+        user_gender = self.user_gender_combobox.currentText()
+        if user_gender == "男" and user_like == "男":
             QMessageBox.information(self, "提示", "你是Gay", QMessageBox.Yes)
-        elif selected_text == "女":
+        elif user_gender == "男" and user_like == "女":
             QMessageBox.information(self, "提示", "你不是Gay", QMessageBox.Yes)
         else:
-            QMessageBox.information(self, "提示", "?", QMessageBox.Yes)
+            QMessageBox.information(self, "提示", "女的检测干嘛？", QMessageBox.Yes)
 
 if __name__ == '__main__':
     app = QApplication()
